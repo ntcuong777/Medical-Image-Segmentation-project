3193a86bc202b9a16eeb7da18cf321dde5436cf5
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2021-06-15 11:50:36 +0700

    Update dice_focal_loss.py

--- module/losses/dice_focal_loss.py
+++ module/losses/dice_focal_loss.py
@@ -1,4 +1,4 @@
-from .dice_loss import DiceLossWithLogits
+from .dice_loss import DiceLoss
 from .focal_loss import FocalLoss
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,7 +7,7 @@ class DiceFocalLoss(nn.Module):
     def __init__(self): # temporarily, no parameters
         super(DiceFocalLoss, self).__init__()
         self.my_focal = FocalLoss()
-        self.my_dice = DiceLossWithLogits()
+        self.my_dice = DiceLoss()
     
     def forward(self, inputs, targets):
         return self.my_focal(inputs, targets) + self.my_dice(inputs, targets)
