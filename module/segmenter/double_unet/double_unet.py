8790d6220d7868012146af4e55f38772471599f0
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2021-07-01 18:20:55 +0700

    Update double_unet.py

--- module/segmenter/double_unet/double_unet.py
+++ module/segmenter/double_unet/double_unet.py
@@ -12,7 +12,7 @@ class DoubleUnet(nn.Module):
         self.last_conv = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, padding='same')
     
     def forward(self, inputs, use_sigmoid=False):
-        first_net_out, first_net_enc = self.first_network(inputs)
+        first_net_out, first_net_enc = self.first_network(inputs, double_unet_style=True)
         x = torch.cat((first_net_out, inputs), dim=1) # I will concatenate by depth instead of multiplying
         
         second_net_out = self.second_network(x, first_net_enc)
