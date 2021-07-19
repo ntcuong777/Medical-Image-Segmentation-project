c889377856c5f304012251344b25cb89a1db7195
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2021-07-19 19:41:42 +0700

    Update more

--- Expr.py
+++ Expr.py
@@ -17,4 +17,5 @@ if __name__ == "__main__":
 
     train(opt)
     test(opt)
+    speedtest(opt)
     eval(opt)
--- run/__init__.py
+++ run/__init__.py
@@ -1,3 +1,4 @@
 from .Train import train
 from .Test import test
 from .Eval import eval
+from .speedtest import speedtest
